031c1d1667e87229cc7ca1a37dbb8291711f008e
MinHyup1 <88864163+MinHyup1@users.noreply.github.com>
2021-08-19 11:35:43 +0900

    Add files via upload

--- resources/js/bg.js
+++ resources/js/bg.js
@@ -1,23 +1,17 @@
-/**
- * 
- */
-
-let getBackgroundImage = async ()=>{
+let getBackgrundImage = async () => {
 	
-		let prevBackground = JSON.parse(localStorage.getItem('bg-log'));
-		
-		if(prevBackground && (prevBackground.expiresOn >  Date.now())) {
-			return prevBackground.imgInfo;
-		}
+	let prevBackground = JSON.parse(localStorage.getItem('bg-log'));
 	
-		
-		let imgObject = await requestBackground();
-		insertBackgroundLog(imgObject);
-		return imgObject;
-		
+	if(prevBackground && (prevBackground.expiresOn > Date.now())){
+		return prevBackground.imgInfo;
+	}
+	
+	let newBackground = await requestBackground();
+	insertBackgroundLog(newBackground);
+	return newBackground;
 };
 
-let requestBackground = async () =>{
+let requestBackground =  async () => {
 	
 	let params = {
 		orientation:'landscape',
@@ -25,95 +19,80 @@ let requestBackground = async () =>{
 	}
 	
 	let queryString = createQueryString(params);
-	 
-	let response = await fetch('https://api.unsplash.com/photos/random?'+ queryString ,{
-					method:'get',
-					headers:{Authorization: 'Client-ID kY2CM42VK0U4H8ovy-Xucb8Dd7OcswJmIPgSuOlHZ48'}
-				});
+	let response = await fetch('https://api.unsplash.com/photos/random?'+ queryString,{
+							method:'get',
+							headers:{Authorization: 'Client-ID EpGHJm1CAHxvGARpBDkUJdsCsoeO2zGU011g0AXO4Xo'}
+						});
 	let obj = await response.json();
-		
 	return {
-			img : obj.urls.full,
-			place : obj.location.title
-		}
+		img : obj.urls.full,
+		place: obj.location.title
+	}
 }
 
-let insertBackgroundLog = (imgObject) => {
-	let expirationDate = new Date()
-		expirationDate = expirationDate.setSeconds(expirationDate.getSeconds() + 10);
-		
-		
-		const backgroundLog = {
-			expiresOn : expirationDate,
-			imgInfo : imgObject
-		}
-		
-		localStorage.setItem('bg-log',JSON.stringify(backgroundLog));
+let insertBackgroundLog = (newBackground) => {
 	
+	let expirationDate = new Date();
+	expirationDate = expirationDate.setDate(expirationDate.getDate() + 1);
+
+	const backgroundLog = {
+		expiresOn : expirationDate,
+		imgInfo : newBackground
+	}
+	
+	localStorage.setItem('bg-log',JSON.stringify(backgroundLog));
+}	
+	
+let getCoords = () => {
+	if(!navigator.geolocation) {
+		 return new Promise((resolve,reject)=>{
+			 reject();
+		  });
+	}else{
+		  return new Promise((resolve,reject)=>{
+			  navigator.geolocation.getCurrentPosition((position) => {
+				  resolve(position.coords);
+			  });
+		  })
+    }
 }
 
-
-	
-let setCurrentPositionInfo = async () => {
-	let coords = await getCoords();
-	
-	
-}
-
-let getLocationTemp = async ()=>{
-		const OPEN_WATHER_API_KEY = '83b95d9b8f39524ac69a9c4834ff1553';
+let getLocationTemp = async () => {
+		const OPEN_WEATHER_API_KEY = 'f8623f0692e3030bc42fc7941ade2c93';
 		let coords = await getCoords();
-		
+
 		let params = {
 				lat:coords.latitude,
 				lon:coords.longitude,
-				appid:OPEN_WATHER_API_KEY,
+				appid:OPEN_WEATHER_API_KEY,
 				units:'metric',
 				lang:'kr'
 		};
+		
 		let queryString = createQueryString(params);
 		let url = `https://api.openweathermap.org/data/2.5/weather?${queryString}`;
 		
-		let response = await fetch(url,{
-			method : 'get'
-		});
-		let obj = await response.json();
-		console.dir(obj);
-		return{
-			temp : obj.main.temp,
-			palce : obj.name
-		}
+		let response = await fetch(url);
+		let obj =  await response.json();
 		
+		return {
+			temp : obj.main.temp,
+			place : obj.name
+		}
 	}
 
-  let getCoords = () => {
-      if(!navigator.geolocation) {
-          return new Promise((resolve,reject)=>{	
-             reject();
-           });
-      }else{
-           return new Promise((resolve,reject)=>{
-              navigator.geolocation.getCurrentPosition((position) => {
-                 resolve(position.coords);
-              });
-           })
-       }
-   }
-
 (async ()=>{
+	/* 배경 이미지와 이미지의 위치정보 랜더링 */
+	let background = await getBackgrundImage();
+	document.querySelector('body').style.backgroundImage = `url(${background.img})`;
+	if(background.place){
+		document.querySelector('.footer_text').innerHTML = background.place;
+	}
 	
-	/* 배경이미지와 이미지의 위치정보 랜더링*/
-	let background = await getBackgroundImage();
-	document.querySelector('body').style.backgroundImage=`url(${background.img})`;
-	document.querySelector('.footer_test').innerHTML = background.place;
-	/* 지역과 기온 랜더링 */
-	let locationTemp = await getLocationTemp();
-	document.querySelector('.location_text').innerHTML = locationTemp.temp + 'º  @' + locationTemp.place;
+	/* 지역과 기온 랜더링*/
+	let locataionTemp = await getLocationTemp();
+	document.querySelector('.location_text').innerHTML = locataionTemp.temp + 'º @ ' + locataionTemp.place;
 })();
-
-
-
-
 
 
 
--- resources/js/main.js
+++ resources/js/main.js
@@ -1,83 +1,153 @@
 let renderCurrentTime = () => {
-	
 	let now = new Date();
 	let hour = now.getHours();
 	let minutes = now.getMinutes();
 	let seconds = now.getSeconds();
 	
-	hour = hour < 10 ? "0" + hour:hour;
+	hour = hour < 10 ? "0"+hour:hour;
 	minutes = minutes < 10 ? "0" + minutes:minutes;
 	seconds = seconds < 10 ? "0" + seconds:seconds;
-	
-	document.querySelector('.clock_text').innerHTML = `${hour}: ${minutes}: ${seconds}`;
-	
+	document.querySelector('.clock_text').innerHTML = `${hour} : ${minutes} : ${seconds}`;
 }
 
-
-
 let renderUser = e => {
-		e.preventDefault();
-		
-		let inpName = document.querySelector('.inp_name')
-		let username = inpName.value;
-		localStorage.setItem('username',username);
-		
-		convertMainDiv(username);
-	
+	e.preventDefault();
+	console.dir(e.target.className);
+	let inpName = document.querySelector('.inp_name');
+	let username = inpName.value;
+	localStorage.setItem('username',username);
+	convertMainDiv(username);
 }
 
 let convertMainDiv = username => {
 	let inpName = document.querySelector('.inp_name');
-		document.querySelector('.frm_name').className = 'frm_todo';
-		document.querySelector('.username').innerHTML = username;
-		inpName.value = '';
-		inpName.placeholder = '할 일을 입력하세요.';
-		inpName.className = 'inp_todo';
-		
-		document.querySelector('.todo-list').style.display = 'flex';
-		doucment.querySelector('.main').style.justifyContent = 'space-between';
-		doucment.querySelector('.wrap_user').className = 'wrap_todo';
+	
+	document.querySelector('.frm_name').className = 'frm_todo';
+	document.querySelector('.username').innerHTML = username;
+	inpName.value = '';
+	inpName.placeholder = 'Enter your schedule';
+	inpName.className = 'inp_todo';
+	
+	document.querySelector('.todo-list').style.display ='flex';
+	document.querySelector('.main').style.justifyContent ='space-between';
+	document.querySelector('.wrap_user').className = 'wrap_todo';
+	
+	document.querySelector('.frm_todo').addEventListener('submit',registSchedule);
+	document.querySelector('#leftArrow').addEventListener('click',renderPrevPage);
+	document.querySelector('#rightArrow').addEventListener('click',renderNextPage);
 }
 
-let registSchedule = () => {
-	let todoList = JSON.parse(localStorage.getItem('todo'));
+let registSchedule = (e) => {
+	e.preventDefault();
+	let todoList = localStorage.getItem('todo'); //
 	let input = document.querySelector('.inp_todo').value;
+	document.querySelector('.inp_todo').value = '';
 	
-	if(todoList) {
-		prevWork.push({work:input});
+	if(todoList){
+		todoList = JSON.parse(todoList);		
+		let lastIdx = localStorage.getItem('lastIdx');
+		
+		lastIdx = Number(lastIdx) + 1;
+		localStorage.setItem('lastIdx',lastIdx);
+		
+		todoList.unshift({work:input, idx:lastIdx});//여기
+		localStorage.setItem('todo',JSON.stringify(todoList));
+	}else{
+		
+		localStorage.setItem('lastIdx',0);
+		todoList = [{work:input, idx:0}];
 		localStorage.setItem('todo',JSON.stringify(todoList));
 	}
-	else{
-		let todoList = [{work:input}];
-		localStorage.setItem('todo',JSON.stringify(todoList));
-	}
 	
-	
-	renderSchedule(todoList);
+	renderSchedule(todoList.slice(0,7));
 }
 
 let renderSchedule = (todoList) => {
-	document.querySelectorALL('.todo-list>div').forEach(e =>{e.remove()});
-	todoList.forEach(e =>{
+	document.querySelectorAll('.todo-list>div').forEach(e => {e.remove()});
+	
+	todoList.forEach(e => {
 		let workDiv = document.createElement('div');
-		workDiv.innerHTML = e.work;
+		workDiv.innerHTML = '<i class="far fa-trash-alt" data-idx="'+e.idx+'" ></i>  ' + e.work;
 		document.querySelector('.todo-list').appendChild(workDiv);
 	})
+	
+	document.querySelectorAll('.todo-list>div>i').forEach(e => {
+		e.addEventListener('click', removeWork)
+	})
+	
+	document.querySelector('.current-page').textContent = 1;
 }
 
-(()=>{
-	let username = localStorage.getItem('username');
+let removeWork = (event) => {
+	let todoList = JSON.parse(localStorage.getItem('todo'));
+	
+	let curPage = document.querySelector('.current-page').textContent;
+	let renderPage = curPage;
+	let end = renderPage * 8;
+	let begin = end - 8;
+	
+	let res = todoList.filter(e => {
+		let removeIdx = event.target.dataset.idx;
+		return removeIdx != e.idx;
+	})
+	
+	localStorage.setItem('todo',JSON.stringify(res));
+	renderSchedule(res.slice(begin,end));
+}
+
+let renderPrevPage = () => {
+	//현재 페이지
+	let curPage = document.querySelector('.current-page').textContent;
+	let todoList = JSON.parse(localStorage.getItem('todo'));
+
+	if(curPage < 2){
+		alert("첫 페이지 입니다.");
+		return;
+	}
+	
+	let renderPage = curPage - 1;
+	let end = renderPage * 8;
+	let begin = end - 8;
+	
+	renderSchedule(todoList.slice(begin,end));
+	document.querySelector('.current-page').textContent = renderPage;
+}
+
+let renderNextPage = () => {
+	
+	//현재 페이지
+	let curPage = document.querySelector('.current-page').textContent;
+	let todoList = JSON.parse(localStorage.getItem('todo'));
+	let lastPage = Math.ceil(todoList.length/8);
+	
+	
+	if(curPage >= lastPage){
+		alert("마지막 페이지 입니다.");
+		return;
+	}
+	
+	let renderPage = Number(curPage) + 1;
+	let end = renderPage * 8;
+	let begin = end - 8;
+	
+	renderSchedule(todoList.slice(begin,end));
+	document.querySelector('.current-page').textContent = renderPage;
+	
+}
+
+(()=> {
+	let username = localStorage.getItem('username');	
+	let todoList = localStorage.getItem('todo');//수정	
 	
 	if(username){
 		convertMainDiv(username);
-		document.querySelector('.frm_todo').addEventListener('submit',registSchedule);
 	}else{
 		document.querySelector('.frm_name').addEventListener('submit',renderUser);
 	}
 	
+	if(todoList){
+		renderSchedule(JSON.parse(todoList).slice(0,7));
+	}
 	setInterval(renderCurrentTime,1000);
-	
-})();
-
-
+})()
 
